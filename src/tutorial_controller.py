0d0601ee20c78091f1a48de7e06bdf3a337a1393
peter0201yu <peter0201yu@gmail.com>
2022-03-25 15:33:04 -0400

    clean up

--- src/tutorial_controller.py
+++ src/tutorial_controller.py
@@ -6,58 +6,39 @@ from geometry_msgs.msg import Twist, PoseStamped
 
 goal_pose = None
 goal_msg = None
-get_pos_once = False
 def goal_callback(msg):
     global goal_pose, goal_msg
     goal_pose = msg.pose.position
     goal_msg = msg
-
+    #print(goal_msg.header.frame_id)
 
 if __name__ == '__main__':
     rospy.init_node('simple_controller')
-    listener = tf.TransformListener()
-
     rospy.Subscriber("/move_base_simple/goal", PoseStamped, goal_callback)
 
+    listener = tf.TransformListener()
     cmd_vel_publisher = rospy.Publisher('/mobile_base_controller/cmd_vel', Twist,queue_size=1)
 
-    rate = rospy.Rate(10.0)
-
-    angle = 0
     while not rospy.is_shutdown():
         if not goal_pose:
             continue
         try:
-            #(goal_trans,goal_rot) = listener.lookupTransform(goal.header.frame_id, '/map', rospy.Time(0))
-            (robot_trans,robot_rot) = listener.lookupTransform('/base_link', goal_msg.header.frame_id, rospy.Time(0))
+            (robot_trans,robot_rot) = listener.lookupTransform(goal_msg.header.frame_id, '/base_link',  rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-        
-        # print("goal: ", goal_pose)
-        # print("robot: ", robot_trans, robot_rot)
 
-        if not get_pos_once:
-            robot_to_goal = [goal_pose.x - robot_trans[0], goal_pose.y - robot_trans[1]]
-            print("robot to goal: ", robot_to_goal)
-            angle = math.atan2(robot_to_goal[1], robot_to_goal[0])
-            # get_pos_once = True
-            
-        orientation = tf.transformations.euler_from_quaternion(robot_rot)[2]
-        # if orientation < 0:
-        #     orientation += math.pi * 2
+        robot_to_goal = (goal_pose.x - robot_trans[0], goal_pose.y - robot_trans[1])
+        angle = math.atan2(robot_to_goal[1], robot_to_goal[0]) # -pi to pi
+        orientation = tf.transformations.euler_from_quaternion(robot_rot)[2] # -pi to pi
 
-        print("angle: ", angle * 180/math.pi )
-        print("robot orientation: ", orientation * 180/math.pi)
-        angular = (orientation - angle) * 0.1 #-0.5 * angle
         linear = 0
-
-
+        angular = (angle - orientation) * 0.2 #-0.5 * angle
+        if abs(angular) < 0.05:
+            linear = 0.5
 
         cmd = Twist()
-        cmd.linear.x = 0
+        cmd.linear.x = linear
         cmd.angular.z = angular
-
-        print("cmd_vel: ", cmd)
         cmd_vel_publisher.publish(cmd)
 
-        rate.sleep()
+        rospy.Rate(10.0).sleep()
